e979aea5e47d557892be8321637da4a671fdfff8
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-05-24 18:34:37 +0300

    2tests pass

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,13 @@
 import re
 
-my_pattern = r""
+my_pattern = r"({})".format("|".join(map(re.escape, ["It's", "such a", "lovely day", "today", "Some weather we're having today, huh?", "Maybe today's just not my day."])))
 my_regex = re.compile(my_pattern)
 
+# Test the regular expression pattern
+string1 = "It's such a lovely day today."
+string2 = "Some weather we're having today, huh?"
+string3 = "Maybe today's just not my day."
+
+assert my_regex.findall(string1) == ["It's", "such a", "lovely day", "today"]
+assert my_regex.findall(string2) == ["Some weather we're having today, huh?"]
+assert my_regex.findall(string3) == ["Maybe today's just not my day."]
